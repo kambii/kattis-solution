81504138c4d39a6793b591bdcde134f1e1d5d9b7
Muhindo Kambale <kambale.muhindo@scania.com>
2023-10-24 10:28:17 +0200

    public class velkomin{ sloution

---
+++ src/main/java/org/example/velkomin/velkomin.java
@@ -0,0 +1,11 @@
+package org.example.velkomin;
+
+public class velkomin{
+    public static void output(){
+        System.out.println("velkomin!");
+    }
+
+    public static void main(String[] args){
+        output();
+    }
+}
