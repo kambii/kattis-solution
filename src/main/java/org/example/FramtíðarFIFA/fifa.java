822982d75c66628489744bd2e8ce7546dc1d8a18
Muhindo Kambale <kambale.muhindo@scania.com>
2023-10-24 15:12:48 +0200

    fifa solution

---
+++ src/main/java/org/example/FramtíðarFIFA/fifa.java
@@ -0,0 +1,20 @@
+package org.example.FramtíðarFIFA;
+
+import java.util.Scanner;
+
+public class fifa{
+    public void put(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  // the number of improvements since you were frozen.
+        int k = sc.nextInt();  // the number of improvements the game receives every year.
+
+        int currentYear = 2022 + (n / k);
+        System.out.println(currentYear);
+
+    }
+    public static void main(String[] args) {
+
+        fifa year = new fifa();
+        year.put();
+    }
+}
