89f069e1734a90fe32c8631e1370886c9fa8ad96
Muhindo Kambale <kambale.muhindo@scania.com>
2023-10-25 11:11:31 +0200

    apaxiaaans solution

---
+++ src/main/java/org/example/Apaxiaaaaaaaaaaaans/apaxiaaans.java
@@ -0,0 +1,25 @@
+package org.example.Apaxiaaaaaaaaaaaans;
+
+import java.util.Scanner;
+
+public class apaxiaaans {
+    public static String input(String name){
+        StringBuilder result = new StringBuilder(); // Used to build and manipulate strings efficiently
+        char previousChar = 0;
+
+        for (char currentChar : name.toCharArray()) {
+            if (currentChar != previousChar) {
+                result.append(currentChar);
+                previousChar = currentChar;
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        String compactName = input(name);
+        System.out.println(compactName);
+    }
+}
